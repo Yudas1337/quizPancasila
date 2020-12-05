4f4fef8d0984d3d1d3c59c8af7d160e4bfe060ed
wira29 <bag4sk4r4@gmail.com>
2020-12-06 02:41:25 +0700

    Update list_soal.php

--- dashboard/list_soal.php
+++ dashboard/list_soal.php
@@ -43,88 +43,88 @@ require_once __DIR__ . "/../templates/navbar.php";
             </div>
         </div>
         <div class="row mt-3">
-        <div class="col-md-12">
-            <table class="table table-bordered table-striped" id="datatable" style="width:100%">
-            <thead class="thead-dark">
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Soal</th>   
-                <th scope="col">Kunci</th>
-                <th scope="col">Nilai</th>
-                <th scope="col">Status</th>
-                <th scope="col">Gambar</th>
-                <th scope="col">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php 
-                $no = 1;
-                foreach($soal as $so): ?>
-                <tr>
-                <th scope="row"><?= $no ?></th>
-                    <td><?= $so['isiSoal'] ?></td>
-                    <td><?= $so['kunci_jwb'] ?></td>
-                    <td><?= $so['nilaiSoal'] ?></td>
-                    <td>
-                        <?php if($so['status'] == 1){
-                            echo '<span class="badge badge-success">Aktif</span>';
-                        }else{
-                            echo '<span class="badge badge-danger">Non-aktif</span>';
-                        } ?>
-                    </td>
-                    <td><img src="<?= $init->base_url('assets/img/soal/'.$so['fotoSoal']) ?>" alt=""></td>
-                    <td>
-                        <button class="btn btn-warning btn-sm text-white"><i class="fas fa-edit"></i></button> |
-                        <?php
-                        if($so['status'] == 1){
-                            echo '<button class="btn btn-danger btn-sm" type="button" data-toggle="modal" data-target="#modalStatus"><i class="fas fa-eye-slash"></i></button>';
-                        }else{
-                            echo '<button class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#modalStatus"><i class="fas fa-eye"></i></button>';
-                        }
-                        ?>
-                    </td>
-                </tr>
+            <div class="col-md-12">
+                <table class="table table-bordered table-striped" id="datatable" style="width:100%">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Soal</th>   
+                    <th scope="col">Kunci</th>
+                    <th scope="col">Nilai</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Gambar</th>
+                    <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                    $no = 1;
+                    foreach($soal as $so): ?>
+                    <tr>
+                    <th scope="row"><?= $no ?></th>
+                        <td><?= $so['isiSoal'] ?></td>
+                        <td><?= $so['kunci_jwb'] ?></td>
+                        <td><?= $so['nilaiSoal'] ?></td>
+                        <td>
+                            <?php if($so['status'] == 1){
+                                echo '<span class="badge badge-success">Aktif</span>';
+                            }else{
+                                echo '<span class="badge badge-danger">Non-aktif</span>';
+                            } ?>
+                        </td>
+                        <td><img src="<?= $init->base_url('assets/img/soal/'.$so['fotoSoal']) ?>" alt=""></td>
+                        <td>
+                            <button class="btn btn-warning btn-sm text-white"><i class="fas fa-edit"></i></button> |
+                            <?php
+                            if($so['status'] == 1){
+                                echo '<button class="btn btn-danger btn-sm" type="button" data-toggle="modal" data-target="#modalStatus"><i class="fas fa-eye-slash"></i></button>';
+                            }else{
+                                echo '<button class="btn btn-info btn-sm" type="button" data-toggle="modal" data-target="#modalStatus"><i class="fas fa-eye"></i></button>';
+                            }
+                            ?>
+                        </td>
+                    </tr>
 
-                <!-- Modal -->
-                    <div class="modal fade" id="modalStatus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                    <form method="POST" action="">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Soal</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            
-                            <div class="modal-body">
-                                <input type="hidden" name="idSoal" value="<?= $so['idSoal']?>">
-                                <input type="hidden" name="status" value="<?= $so['status']?>">
-                                <?php
-                                if($so['status'] == 1){
-                                    echo 'Apakah anda ingin menyembunyikan soal ini ?';
-                                }else{
-                                    echo 'Apakah anda ingin menampilkan soal ini ?';
-                                }
-                                ?>
+                    <!-- Modal -->
+                        <div class="modal fade" id="modalStatus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                        <form method="POST" action="">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Soal</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                
+                                <div class="modal-body">
+                                    <input type="hidden" name="idSoal" value="<?= $so['idSoal']?>">
+                                    <input type="hidden" name="status" value="<?= $so['status']?>">
+                                    <?php
+                                    if($so['status'] == 1){
+                                        echo 'Apakah anda ingin menyembunyikan soal ini ?';
+                                    }else{
+                                        echo 'Apakah anda ingin menampilkan soal ini ?';
+                                    }
+                                    ?>
+                                    
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+                                    <button type="submit" name="submit" class="btn btn-primary">Ya</button>
+                                </div>
                                 
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
-                                <button type="submit" name="submit" class="btn btn-primary">Ya</button>
-                            </div>
-                            
+                            </form>
                         </div>
-                        </form>
-                    </div>
-                    </div>
-                <!-- End Modal -->
-                <?php $no++;endforeach ?>
-            </tbody>
-            
+                        </div>
+                    <!-- End Modal -->
+                    <?php $no++;endforeach ?>
+                </tbody>
+                
+                </div>
             </div>
         </div>
-    </div>
     </div>
     
 
