024886a45908647a9e92881a57de6104a6287207
Yudas <yudasmalabi@gmail.com>
2020-12-06 11:23:29 +0700

    rest server update

---
+++ rest/RestController.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . "/../configuration.php";
+class RestController extends Config
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function restSoal()
+    {
+        $sql = $this->db->query("SELECT * FROM qz_soal JOIN qz_jawaban ON qz_soal.idSoal = qz_jawaban.idSoal");
+        $arr = array();
+
+        while ($data = $sql->fetch_object()) {
+
+            array_push($arr, array(
+                'idSoal' => $data->idSoal,
+                'isiSoal' => $data->isiSoal,
+                'kunci_jwb' => $data->kunci_jwb,
+                'fotoSoal' => (!$data->fotoSoal) ? '' : $data->fotoSoal,
+                'nilaiSoal' => $data->nilaiSoal,
+                'statusSoal' => $data->statusSoal,
+                'jawaban' => array(
+                    'idJawaban' => $data->idJawaban,
+                    'idSoal'    => $data->idSoal,
+                    'uraian'    => $data->uraian,
+                    'abjad'     => $data->abjad
+                )
+            ));
+        }
+
+        echo json_encode(array(
+            'value' => 1,
+            'result' => $arr
+        ));
+
+        $this->db->close();
+    }
+}
+
+$rest = new RestController;
